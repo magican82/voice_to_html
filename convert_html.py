e9b4ca659b42f4f2ffe43e0fbc9aca750dff6ee4
magican82 <47121390+magican82@users.noreply.github.com>
2024-04-02 11:35:51 +0300

    Add files via upload

---
+++ convert_html.py
@@ -0,0 +1,79 @@
+import whisper
+from pyannote.audio import Pipeline
+from combine import diarize_text
+from pydub import AudioSegment
+import os
+import torch
+
+#sound = AudioSegment.from_mp3("test_sber.mp3")
+#sound.export("audio.wav", format="wav")
+
+def recogonize_speach(f,path,dest_path):
+    pipeline = Pipeline.from_pretrained(
+    "pyannote/speaker-diarization")
+
+
+    #pipeline.to(torch.device("cuda")) Enable it to increase speed with cuda GPU
+
+    # run the pipeline on an audio file
+    diarization = pipeline(path)
+
+
+
+    model = whisper.load_model('medium')
+    asr_result = model.transcribe(path)
+
+    final_result = diarize_text(asr_result, diarization)
+
+    html_file_name = os.path.abspath(os.curdir)+'\\html\\' + os.path.splitext(os.path.basename(f))[0]+'.html'
+    f_html = open(html_file_name,'w')
+    from header import print_header,print_footer
+    line = print_header(dest_path)
+    print(line)
+    f_html.write(line)
+    f_html.write("\n")
+    
+    for seg, spk, sent in final_result:
+        line =f'<tr><th>{seg.start:.2f}</th><th2.00</th><th>{spk}<button onclick="audio=document.getElementById(\'player\'); audio.currentTime={seg.start:.2f}; document.getElementById(\'player\').play()">...</button> </th><th>{sent}</th>'
+        #f'{seg.start:.2f} {seg.end:.2f} {spk} {sent}'
+        print(line)
+        f_html.write(line)
+        f_html.write("\n")
+
+    line = print_footer()    
+    print(line)
+    f_html.write(line)
+    f_html.write("\n")
+    f_html.close()
+
+
+def print_HTML(filename):
+     f = open (filename, 'rb')
+     linex=''
+     for str_ in f:
+         str_utf=str_.decode('utf8')
+         print(str_utf)
+         filename=linex+str_utf
+     return linex
+
+for f in os.scandir('wav'):
+    if f.is_file() and f.path.split('.')[-1].lower() == 'wav':
+        with open(f.path, 'r') as csvfile:
+            destination = os.path.abspath(os.curdir)+'\\wav\\converted\\'+os.path.basename(f.path)
+            recogonize_speach(f,f.path,destination)
+            
+            
+        os.rename(f.path, destination)
+
+
+ 
+
+
+
+
+    
+    
+
+
+
+
---
+++ header.py
@@ -0,0 +1,32 @@
+def print_header(mediapath):
+    header = '<head>\n'\
+    '<style>\n'\
+    'th, td {\n'\
+    '  border-style:dotted;\n'\
+    '  border-color: #96D4D4;\n'\
+    '  text-align: left;\n'\
+    '  font-weight: normal;\n'\
+    '  font-size: medium;\n'\
+    '  font-style: normal;\n'\
+    '}\n'\
+    '</style>\n'\
+    '</head>\n'\
+    '<body>\n'\
+    '<br />\n'\
+    '<audio id="player" src="'+mediapath+'"></audio>\n'\
+    '<br />\n'\
+    '<div>\n'\
+    '    <button onclick="document.getElementById(\'player\').volume+=0.1">vol +</button>\n'\
+    '    <button onclick="document.getElementById(\'player\').volume-=0.1">vol -</button>\n'\
+    '</div>\n'\
+    '<table>\n'
+    #print(header) 
+    return header
+
+def print_footer():
+    footer = '</table>footer\n'\
+    '</body>\n'
+    #print(footer) 
+    return footer
+#print_header("C:\python\voicercognition\wav\converted\2024013401_84959260954.wav")
+print_footer()
